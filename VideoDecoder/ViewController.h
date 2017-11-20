51c39e8b275c0b91db151798a09897ae6214a3bb
imp <464553111@qq.com>
2017-11-20 10:30:41 +0800

    Initial Commit

---
+++ VideoDecoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoDecoder
+//
+//  Created by Imp on 2017/11/20.
+//  Copyright © 2017年 codoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoDecoder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoDecoder
+//
+//  Created by Imp on 2017/11/20.
+//  Copyright © 2017年 codoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
